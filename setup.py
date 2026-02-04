0c82e3e94cfe29429d79a0d8fe691b6909770066
Pablo.Filgueira <pablo.filgueira@brubank.com>
2026-02-04 17:15:03 -0300

    Match package name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="robusta-holmesgpt-playbooks",
+    name="holmesgpt_playbooks",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
